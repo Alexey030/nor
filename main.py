bbaf8bf66095ae3a4b33988784d05f9d2a3fa094
Alexey030 <kbahtoff@mail.ru>
2022-11-05 19:56:34 +0300

    Задание 3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-a=int(input("Введите длинну прямоугольника: "))
-b=int(input("Введите ширину прямоугольника: "))
-for i in range(a):
-    for c in range(b):
-        print("*",end=" ")
-    print()
+a=int(input("Введите длинну: "))
+b=int(input("Введите ширину: "))
+j=input("Введите символ: ")
+print(j*a)
+for i in range(b-2):print(j," "*(a-2),j,sep="")#*a-2 для того чтобы получить пустоту внутри прямоугольника
+print(j*a)
