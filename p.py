95b4987ea65fc94dd3930eb9cb9e8c7e41fe79f0
Alexey030 <kbahtoff@mail.ru>
2022-11-12 22:04:14 +0300

    задание 3 наверное рабочее

--- p.py
+++ p.py
@@ -1,2 +1,9 @@
-a=input("Введите текст: ")
-print(a.count("."))
+с = input()
+l = len(с)
+l = l//2
+for i in range(l):
+    if с[i]!=с[-1-i]:
+        print("нет")
+        break
+else:
+    print("да")
