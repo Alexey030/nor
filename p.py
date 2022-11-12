5bd49e555dcc3025c52994d7aaabf19d854b2562
Alexey030 <kbahtoff@mail.ru>
2022-11-12 21:23:22 +0300

    задание 3

--- p.py
+++ p.py
@@ -1,2 +1,9 @@
-for a in reversed("python"):
-    print(a)
+с = input()
+l = len(с)
+l = l//2
+for i in range(l):
+    if с[i]==с[-1-1-1]:
+        print("нет")
+        break
+else:
+    print("да")
